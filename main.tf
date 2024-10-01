5d7fe8fa0b6c3006b6dbcf693732e7ca29cd96ab
sgandeplli <sgandepalli@softility.com>
2024-10-01 11:09:48 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,22 @@
+provider "google" {
+  project = 'primal-gear-436812-t0'
+  region = 'us-central1'
+}
+
+resource "google_compute_instance" "centos_vm {
+  name = 'sekhar_instance'
+  machine_type = 'e2-medium'
+  zone = 'us-central1-a'
+
+  boot_disk {
+    initialize_params {
+      image = "centos-cloud/centos-stream-9"  # Correct image family
+    }
+  }
+
+   network_interface {
+    network = "default"
+    access_config {
+    }
+  }
+}
